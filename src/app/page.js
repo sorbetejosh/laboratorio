405005860803cf4ff67e82ef1451c7de5d128b1a
CCB0\BACO <alangonzalez9407@gmail.com>
2023-09-27 15:47:59 -0600

    Add info section

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main>
+    <main className={styles.main}>
       <header className={styles.header}>
         <div>
           <h1>
@@ -15,10 +15,38 @@ export default function Home() {
               <span>I am</span>
             </span>
             <br />
-            <span>mario man primero</span>
+            <span>josh carlson</span>
           </h1>
         </div>
       </header>
+      <section className={styles.infoSection}>
+        <img
+          src="/imagen-para-website.png"
+          alt="imagen"
+          ClassName={styles.image}
+        />   
+        <div className={styles.infoContainer}>
+          <span className={styles.tittle + " " +styles.yellowText}>josh</span>
+          <br />
+          <span className={styles.tittle}>carlson</span>
+        </div>
+        <div> className={styles.list}
+          <ul>
+            <li>
+              <span className={styles.grayText}>Age: </span>27
+             </li>
+             <li>
+            <span className={styles.grayText}>Nationatily: </span>German
+            </li>
+            <li>
+            <span className={styles.grayText}>Skill set: </span> Project Managment and financial Performance
+            </li>
+            <li>
+            <span className={styles.grayText}>Languages: </span> English, German
+            </li>
+          </ul>
+        </div>    
+      </section>
     </main>
   );
 }
